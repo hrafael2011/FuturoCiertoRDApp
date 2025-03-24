67bf42e0233d3b88b85612d3b671988aad7b714a
Hendrick Rafael <tuemail@example.com>
2025-03-23 21:48:20 -0400

    I added new modification in settins

--- futuroCiertoApi/settings.py
+++ futuroCiertoApi/settings.py
@@ -177,10 +177,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = ['http://localhost:5173', 
                         'http://10.0.0.58:5173',  
-                        'https://new-page.futurociertord.org'
-                        #'https://draacostafit.com',
+                        'https://new-page.futurociertord.org',
+                        'https://draacostafit.com',
                         ]
-CORS_ALLOW_ALL_ORIGINS = True 
+#CORS_ALLOW_ALL_ORIGINS = True 
 
 
 
