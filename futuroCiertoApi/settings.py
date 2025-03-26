e4f1c74ab4467b1c09bec78a7c3340ea856cebe0
Hendrick Rafael <tuemail@example.com>
2025-03-26 17:05:18 -0400

    Base url added

--- futuroCiertoApi/settings.py
+++ futuroCiertoApi/settings.py
@@ -16,6 +16,9 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
+# Base Url
+BASE_URL = 'https://draacostafit.com'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
