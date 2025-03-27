5dbe4627b8218b2f2e520f4c38734240cb833427
Hendrick Rafael <tuemail@example.com>
2025-03-27 01:50:37 -0400

    change debuga

--- futuroCiertoApi/settings.py
+++ futuroCiertoApi/settings.py
@@ -29,7 +29,7 @@ BASE_URL = 'https://draacostafit.com'
 SECRET_KEY = 'django-insecure-&=)%5@(0p4(gqw076#8b_f0b46$&^q1&26p7)ck-g^qiqn%+w9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [#'futurociertord.azurewebsites.net',
                  '3.12.151.15',
